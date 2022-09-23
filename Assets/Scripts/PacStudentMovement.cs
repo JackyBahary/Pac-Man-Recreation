247d36b4016636856b0314ba3b2ecf9194e06216
JackyBahary <75601152+JackyBahary@users.noreply.github.com>
2022-09-23 22:03:29 +1000

    Movement Finished

--- Assets/Scripts/PacStudentMovement.cs
+++ Assets/Scripts/PacStudentMovement.cs
@@ -5,11 +5,17 @@ using UnityEngine;
 public class PacStudentMovement : MonoBehaviour
 {
     public Animator animator;
+    public AudioClip movementClip;
+    private AudioSource source;
+    private bool moving = true;
 
     // Start is called before the first frame update
     void Start()
     {
-        
+        source = GetComponent<AudioSource>();
+        source.clip = movementClip;
+        source.PlayOneShot(movementClip);
+        StartCoroutine(playMovementAudio());
     }
 
     // Update is called once per frame
@@ -22,7 +28,7 @@ public class PacStudentMovement : MonoBehaviour
             transform.position += (new Vector3(1, 0, 0) * Time.deltaTime);
         }
         else if (transform.position.x >= -7.5f && transform.position.y >= 9.5f)
-        { 
+        {
             animator.SetBool("RightReady", false);
             animator.SetBool("DownReady", true);
             transform.position += (new Vector3(0, -1, 0) * Time.deltaTime);
@@ -38,6 +44,15 @@ public class PacStudentMovement : MonoBehaviour
             animator.SetBool("LeftReady", false);
             animator.SetBool("UpReady", true);
             transform.position += (new Vector3(0, 1, 0) * Time.deltaTime);
+        }
+    }
+
+    IEnumerator playMovementAudio()
+    {
+        while (moving)
+        {
+            yield return new WaitForSeconds(1.0f);
+            source.PlayOneShot(movementClip);
         }
     }
 }
